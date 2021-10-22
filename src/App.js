cbf610fe2f09eb67f61322be95cc3014ddb9ecc1
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-10-22 12:31:16 +1100

    RETURNED APP RENDER. Added header, main body with AddThoughtForm component and mapping though exisiting thoughts

--- src/App.js
+++ src/App.js
@@ -19,11 +19,30 @@ function App() {
     },
   ]);
 
+  const addThought = (thought) => {
+    setThoughts(prev => [thought, ...prev]);
+  };
+
+  const removeThought = (thoughtIdToRemove) => {
+    setThoughts(prev => prev.filter(thought => (thought.id !== thoughtIdToRemove)));
+  };
+
   return (
     <div className="App">
-      
+      <header><h1>Passing Thoughts</h1></header>
+      <main>
+        <AddThoughtForm addThought={addThought} />
+        <ul className="thoughts">
+          {thoughts.map((thought) => {
+            <Thought key={thought.id} thought={thought} removeThought={removeThought} />
+          })}
+        </ul>
+      </main>
     </div>
   );
 }
 
-export default App;
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
